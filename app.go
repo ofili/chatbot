4e7845be9405222fa722d9a09c2861ab10e2baa3
Lewis <ofili@users.noreply.github.com>
2020-06-21 01:56:38 +0100

    token

--- app.go
+++ app.go
@@ -65,7 +65,7 @@ func VerificationEndpoint(w http.ResponseWriter, r *http.Request) {
 	mode := r.URL.Query().Get("hub.mode")
 	token := r.URL.Query().Get("hub.verify_token")
 
-	if mode != "" && token == os.Getenv("VERIFY_TOKEN") {
+	if mode != "" && token == os.Getenv("2558051434523679|R8T-BcdI7GcfkC6PuaONPQ8O2xY") {
 		w.WriteHeader(200)
 		w.Write([]byte(challenge))
 	} else {
